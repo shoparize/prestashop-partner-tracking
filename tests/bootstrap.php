c2c853e77d58b7308799dc6f25186c9678c59de4
DzmitryU <urbanovich.mslo@gmail.com>
2023-07-18 11:31:03 +0200

    updated module for render products api using library

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 
-// to support reading local files from plugin code
-// eg. see how $this->version is evaluated by bkn_301_payment.php
-define('_PS_MODULE_DIR_', getcwd() . '../../');
-
 if (file_exists(__DIR__ . '/../PrestaShop/tests-legacy')) {
     require_once __DIR__ . '/../PrestaShop/tests-legacy/bootstrap.php';
 } else {
