938dcdea87ec8e400f2a4a2759f94b85274fe338
DzmitryU <urbanovich.mslo@gmail.com>
2023-07-18 11:08:01 +0200

    updated module for render products api using library

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,13 +4,13 @@
 // eg. see how $this->version is evaluated by bkn_301_payment.php
 define('_PS_MODULE_DIR_', getcwd() . '../../');
 
-if (file_exists(__DIR__ . '/../../PrestaShop/tests-legacy')) {
-    require_once __DIR__ . '/../../PrestaShop/tests-legacy/bootstrap.php';
+if (file_exists(__DIR__ . '/../PrestaShop/tests-legacy')) {
+    require_once __DIR__ . '/../PrestaShop/tests-legacy/bootstrap.php';
 } else {
-    require_once __DIR__ . '/../../PrestaShop/tests/bootstrap.php';
+    require_once __DIR__ . '/../PrestaShop/tests/bootstrap.php';
 }
 
-require_once __DIR__ . '/../../PrestaShop/config/config.inc.php';
-require_once __DIR__ . '/../../PrestaShop/config/defines_uri.inc.php';
-require_once __DIR__ . '/../../PrestaShop/init.php';
+require_once __DIR__ . '/../PrestaShop/config/config.inc.php';
+require_once __DIR__ . '/../PrestaShop/config/defines_uri.inc.php';
+require_once __DIR__ . '/../PrestaShop/init.php';
 require_once __DIR__ . '/../shoparizepartner.php';
