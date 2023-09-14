4da09088d15e6d94c159f756f1e7eb54dbb475d6
DzmitryU <urbanovich.mslo@gmail.com>
2023-09-14 10:40:15 +0200

    updated php version for github actions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,12 @@
 <?php
 
-if (file_exists(__DIR__ . '/../PrestaShop/tests-legacy')) {
+/*if (file_exists(__DIR__ . '/../PrestaShop/tests-legacy')) {
     require_once __DIR__ . '/../PrestaShop/tests-legacy/bootstrap.php';
 } else {
     require_once __DIR__ . '/../PrestaShop/tests/bootstrap.php';
-}
+}*/
+
+require_once __DIR__ . '/../PrestaShop/tests/bootstrap.php';
 
 require_once __DIR__ . '/../PrestaShop/config/config.inc.php';
 require_once __DIR__ . '/../PrestaShop/config/defines_uri.inc.php';
